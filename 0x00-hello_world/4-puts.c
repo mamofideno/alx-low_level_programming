cfa9128a4dec93109a95581b89d6665fba62207e
Mamo Fideno <mamofideno@gmail.com>
2022-03-10 10:55:11 -0800

    correction on 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - entry point
  *
- * return success
+ * Return:Always 0(Success)
  */
-int main (void){
+int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+  return (0);
 }
