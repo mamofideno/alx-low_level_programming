832bafdb9803403132c489fe234b22682f810138
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 10:18:03 -0800

    betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,19 +3,28 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if(n<0){
-    printf("%i is negative\n", n);
-  }else if(n==0){
-    printf("%i is zero\n", n);
-  }
-  else{
-    printf("%i is positive\n", n);
-  }
-  return (0);
+ int n;
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ /* your code goes there */
+if(n<0)
+{
+printf("%i is negative\n", n);
+}
+else if(n==0)
+{
+printf("%i is zero\n", n);
+}
+else
+{
+printf("%i is positive\n", n);
+}
+return (0);
 }
