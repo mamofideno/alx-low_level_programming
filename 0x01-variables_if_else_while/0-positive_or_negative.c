afafb231dfb5d145b25aae98c80761bd9f31b7cd
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 10:23:35 -0800

    betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n < 0)
 {
-printf ("%i is negative\n", n);
+printf("%i is negative\n", n);
 }
 else if ( n == 0 )
 {
-printf ("%i is zero\n", n);
+printf("%i is zero\n", n);
 }
 else
 {
