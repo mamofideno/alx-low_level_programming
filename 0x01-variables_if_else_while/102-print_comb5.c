3559c2a97936ea61131c8024883961c41b1659f1
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 19:13:27 -0800

    cr

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int ch;
 int s;
 for (ch = '0'; ch <= '9'; ch++)
 {
+for (s = ch + 1; s <= '9'; s++)
+{
 putchar(ch);
 putchar(ch);
 putchar(' ');
-for (s = ch + 1; s <= '9'; s++)
-{
 putchar(ch);
 putchar(s);
 if (!(ch == '8' && s == '9'))
