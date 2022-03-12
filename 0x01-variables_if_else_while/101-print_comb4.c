7b8d32250b70d2b7c543926c6200f43efbe6658f
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 18:29:14 -0800

    digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int ch;
+int s;
+for (ch = '0'; ch <= '9'; ch++)
+{
+for (s = ch+1; s <= '9'; s++)
+{
+putchar(ch);
+putchar(s);
+if (ch != '9')
+{
+putchar(',');
+putchar(' ');
+}
+} 
+}
+putchar('\n');
+return (0);
+}
