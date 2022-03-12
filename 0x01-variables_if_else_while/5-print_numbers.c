78c4f845544a120aec47ab712712a513c68978b1
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 17:23:28 -0800

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
