9ee21acd6ba8998dfc4679250c35ea04555171f4
Mamo Fideno <mamofideno@gmail.com>
2022-03-11 17:18:39 -0800

    remove q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if(!(ch == 'e' || ch == 'q'))
+putchar(ch);
+putchar('\n');
+return (0);
+}
