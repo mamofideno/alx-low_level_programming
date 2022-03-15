1d3c77ef10b15bf1f8705767078a8b02a51da1f4
Mamo Fideno <mamofideno@gmail.com>
2022-03-15 14:17:14 -0700

    add p

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "1-main.h"
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */ 
+int main(void)
+{
+print_alphabet();
+return (0);
+}
