00dfadbc1d4d27af6c9e4581b5286099e7df88c2
Mamo Fideno <mamofideno@gmail.com>
2022-03-15 14:42:08 -0700

    alpha

--- 0x02-functions_nested_loops/print_alphabet.c
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -1,16 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
 /* more headers goes there */
-#include <stdio.h>
 #include "1-main.h"
+#include "main.h"
 /* betty style doc for function main goes there */
 /**
  * main - entry point
  *
  * Return:Always 0(Success)
- */ 
-int main(void)
+ */
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+_putchar('a');  
 }
