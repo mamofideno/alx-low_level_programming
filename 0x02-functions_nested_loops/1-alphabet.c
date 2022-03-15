b1225b88294ddafdd23acedcd94e1d1d180c4b02
Mamo Fideno <mamofideno@gmail.com>
2022-03-15 14:01:16 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "1-main.h"
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
+void print_alphabet()
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+} 
+int main(void)
+{
+print_alphabet();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.h
@@ -0,0 +1,6 @@
+#ifndef _PRINT_ALPHABET_
+#define _PRINT_ALPHABET_
+
+void print_alphabet(void);
+
+#endif
