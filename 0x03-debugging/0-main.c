c12c6b8221d384b8fbcf44e8edfcac03c9572685
Mamo Fideno <mamofideno@gmail.com>
2022-03-17 19:17:39 -0700

    negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+int i;
+i=98;  
+positive_or_negative(i);
+return (0);
+}
