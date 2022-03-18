639a722f1b53bcbbd1a121122015bef232108cd9
Mamo Fideno <mamofideno@gmail.com>
2022-03-17 22:39:02 -0700

    hh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@
 void print_most_numbers(void)
 {
 int ch;
-for (ch = 0; ch <= 9; ch++)
+for (ch = '0'; ch <= '9'; ch++)
 {
-if(ch !=2 && ch != 4)
+if(ch !='2' && ch != '4')
 _putchar(ch);
 }
 _putchar('\n');
