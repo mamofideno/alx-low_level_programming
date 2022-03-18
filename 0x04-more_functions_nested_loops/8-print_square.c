4c1e81f51f61e0fa45bdd6c037f0adddbc89a693
Mamo Fideno <mamofideno@gmail.com>
2022-03-17 17:39:45 -0700

    8-c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,19 +2,23 @@
 #include "main.h"
 /* betty style doc for function main goes there */
 /**
- * print_diagonal - entry point
+ * print_square - entry point
  *
- *@h: diagonal
+ *@h: siize of square
  *
  * Return:Always 0(Success)
  */
-void print_diagonal(int h)
-{
+void print_square(int h)
+{  
 int ch;
+int outer;
+for (outer = 1; outer <= h; outer++)
+{
 for (ch = 1; ch <= h; ch++)
 {
-_putchar('\\');
-} 
+_putchar('#');
+}
+}
 _putchar('\n');
 return;
 }
