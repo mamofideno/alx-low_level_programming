ea35a25e86e63b32a6121e0e8ed7a92593305977
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 07:08:15 -0700

    error correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *print_array - print elements of array
  *
@@ -12,6 +13,6 @@ void print_array(int *a, int n)
 {
 int i;
 for (i=0; i < n-1; i++)
-printf("%i, ", a[i]);
-printf("%i\n", a[i]);
+printf("=%i, ", a[i]);
+printf("=%i\n", a[i]);
 }
