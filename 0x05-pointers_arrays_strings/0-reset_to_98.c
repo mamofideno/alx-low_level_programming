2fecbced00d4496bbef02911a1a8d82df7ac070f
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 05:25:28 -0700

    correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /*
  *
- *reset_to_98: resent the given addres value to 98
+ *reset_to_98 - resent the given addres value to 98
  *
  *@*n is varibale for the address
- *RETURN: the value return void
+ *
+ *Return: the value return void
  */
-void reset_to_98(int *n){
-  *n=98;
-  
+void reset_to_98(int *n)
+{
+*n = 98;  
 }
