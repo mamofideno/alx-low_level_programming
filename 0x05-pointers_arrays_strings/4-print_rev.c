25526c9dbecf9eb4493270c2fccea993ad96353b
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 08:27:07 -0700

    cr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,9 +15,9 @@ int size;
 int i;
 int j;
 char str;
-size=strlen(s);
-j=0;
-for (i = size - 1; i >j; i--)
+size = strlen(s);
+j = 0;
+for (i = size - 1; i >= j; i--)
 {
 str=s[j];
 s[j]=s[i];
@@ -25,5 +25,5 @@ s[i]=str;
 j=j+1;
 }
 printf("%s\n", s);
- return;
+return;
 }
