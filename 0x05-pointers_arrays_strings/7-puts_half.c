b67f933964fe79d051369f27b99b55c2be412f86
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 09:08:57 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,19 @@
 void puts_half(char *s)
 {
 int size;
+int l;
 int i;
 char *str;
-str =NULL;
-size = (int) strlen(s)/2;
-printf("%i",size);
-for (i = 0; i<= size; i++)
+str = NULL;
+size = strlen(s);
+l = (size / 2);
+if( l > 0)
+{
+for (i = 0; i <= l; i++)
 {
 str[i]=s[i];
 }
 printf("%s\n", str);
+}
 return;
 }
