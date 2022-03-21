a7e0a36c220ac1252f5e20b9c8adaec61e146c16
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 05:27:34 -0700

    avoid space in Betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,6 @@
 #include "main.h"
-/*
- *
- *reset_to_98 - resent the given addres value to 98
+/**
+ * reset_to_98 - resent the given addres value to 98
  *
  *@*n is varibale for the address
  *
@@ -9,5 +8,5 @@
  */
 void reset_to_98(int *n)
 {
-*n = 98;  
+*n = 98;
 }
