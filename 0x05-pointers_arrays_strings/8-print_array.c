74833b19a183ea86d06ce8f0c079fd1814a7fe75
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 07:11:17 -0700

    logicl correction

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,14 +5,14 @@
  *
  *@a: pointer to array
  *
- *@b: number of elements is array
+ *@n: number of elements is array
  *
  *Return: void
  */
 void print_array(int *a, int n)
 {
 int i;
-for (i=0; i < n-1; i++)
-printf("=%i, ", a[i]);
-printf("=%i\n", a[i]);
+for (i = 0; i < n - 1; i++)
+printf("%i, ", a[i]);
+printf("%i\n", a[i]);
 }
