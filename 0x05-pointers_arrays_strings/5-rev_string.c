175f2252d5ffdcfcfc98c3d887a56b8b932219f0
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 17:51:11 -0700

    revstr

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
 /**
- *print_rev - print the strings
+ *rev_string - print the strings
  *
  *@s: char to be printed
  *
  *Return: return void
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
-printf("=%s", strrev(s));
+int l;
+int i;
+l = strlen(s);
+if (l > 0)
+{
+for (i = 1; i < l + 1; i++)
+printf("%c", s[l - i]);
+}
+else
+{
+printf("\n");
+}
+return;
 }
