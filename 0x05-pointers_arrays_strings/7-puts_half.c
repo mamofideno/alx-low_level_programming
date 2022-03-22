6467a7c6744137125ae6c0aba898f6db623deacb
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 18:09:28 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,10 @@ int size;
 int l;
 int i;
 size = strlen(s);
-l = (size -1)/ 2;
+if ( size % 2 == 0)
+l = size / 2;
+else
+l = (size -1) / 2;
 if( l > 0)
 {
 for (i = l; i < size; i++)
@@ -23,5 +26,7 @@ for (i = l; i < size; i++)
 printf("%c", s[i]);
 }
 }
+else
+printf("\n");
 return;
 }
