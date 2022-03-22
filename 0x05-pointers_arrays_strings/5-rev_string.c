fb6fc536ae04529f3eebaa311cca422047cf6f98
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 18:38:11 -0700

    5-rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,23 @@
 #include <string.h>
 #include "main.h"
 /**
- *rev_string - print the strings
+ *puts_half - print the strings
  *
  *@s: char to be printed
  *
  *Return: return void
  */
-void rev_string(char *s)
+void puts_half(char *s)
 {
 int l;
 int i;
+int t;
 l = strlen(s);
-for (i = 1; i < l + 1; i++)
-printf("%c", s[l - i]);
+for (i = 1; i < l / 2; i++)
+{
+t = s[i];
+s[i] = s[l - i - 1];
+s[l - i - 1] = t;
+}
 return;
 }
