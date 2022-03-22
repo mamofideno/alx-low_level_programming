429bc3e08809f8df8dd6d38f71bfe91373af39d8
Mamo Fideno <mamofideno@gmail.com>
2022-03-21 20:11:06 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,32 +11,34 @@ int _atoi(char *s)
 {
   int nu;
   int i;
-  int l = strlen(s);
-  int count=0;
+  int l;
+  int count;
   char numb[l];
   int num;
+  count = 0;
+  l = strlen(s);
   num = 1;
   for (i = 0; i < l; i++)
     {
-      if((s[i] == '+' || s[i] == '-' ) && count ==0)
+      if((s[i] == '+' || s[i] == '-' ) && count == 0)
 	{
-	  numb[0]=s[i];
+	  numb[0] = s[i];
 
 	}
       else if(s[i] >= '0' && s[i] <= '9')
 	{
-	  numb[num]=s[i];
+	  numb[num] = s[i];
 	  count ++;
 	  num ++;
 	}
 
     }
   nu=0;
-  for(int i=1;i<count;i++)
+  for(i = 1; i < count; i++)
     {
-      nu=nu*10+numb[i];
+      nu = nu * 10 + numb[i] ;
     }
   if(numb[0] == '-')
-    nu=nu*-1;  
+    nu = nu * -1;  
   return (nu);
-} 
+}
