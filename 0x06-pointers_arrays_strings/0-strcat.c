91734b0a7c998f8edfe45a29d66380ab490a2aff
Mamo Fideno <mamofideno@gmail.com>
2022-03-25 04:23:37 -0700

    rteur

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "main.h"
+#include "stdio.h"
 /**
  *_strcat - string concat
  *@dest: destination
@@ -7,6 +8,6 @@
  *Return: return character
  */
 char * _strcat(char *dest, char *src)
-{
-return strcat(dest, src);
+{  
+return (strcat(dest, src));
 }
