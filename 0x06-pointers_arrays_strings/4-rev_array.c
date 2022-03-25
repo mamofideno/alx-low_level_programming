a31c887b8219e78f4836faa52983eac5722981a8
Mamo Fideno <mamofideno@gmail.com>
2022-03-25 04:45:36 -0700

    betty style

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 int temp;
 int i;
-for(i = 0; i < n/2; i++)
+for(i = 0; i < n / 2; i++)
 {
 temp = a[i];
-a[i] = a[n-i-1];
-a[n-i-1] = temp;
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
 }
 }
