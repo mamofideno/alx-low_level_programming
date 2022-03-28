5a37392c8872d965a8a91beb6e647ac94c7a38aa
Mamo Fideno <MamoFideno@gmail.com>
2022-03-28 13:45:13 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ *_strchr - initilize arra
+ *@s: source
+ *@c: character
+ *Return: Return pointer to destination array
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+int n; 
+n = strlen(s); 
+for (i = 0; i < n; i++)
+{
+if(s[i] == c)
+return (&s[i]);
+}
+return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,11 @@
 #define __MEMSET_H_
 char *_memset(char *s, char b, unsigned int n);
 #endif
+#ifndef __MEMCPY_H_
+#define __MEMCPY_H_
+char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
+#ifndef __STRCHR_H_
+#define __STRCHR_H_
+char *_strchr(char *s, char c);
+#endif
