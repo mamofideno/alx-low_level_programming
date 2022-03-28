a86a94fe6dd0cfbbc5b8f3cc669461536e645a38
Mamo Fideno <MamoFideno@gmail.com>
2022-03-28 14:23:04 +0300

    spn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ *_strspn - strspn 
+ *@s: string 
+ *@accept: accept
+ *Return: bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i;
+char a;
+a = accept[0]; 
+for (i = 0; i < strlen(s); i++)
+{
+if (s[i] == a)
+return (i+1);
+}
+return (0);
+}
