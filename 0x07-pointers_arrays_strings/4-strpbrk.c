ea18dd4e399d978515f82e246a3882895c687e24
Mamo Fideno <MamoFideno@gmail.com>
2022-03-28 14:54:28 +0300

    str

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,15 +7,17 @@
  *@accept: character
  *Return: Return pointer to destination array
  */
-char *_strchr(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 int i;
 int n;
+int outsize;
 int outer;
+outsize = strlen(accept);
 n = strlen(s);
-for(outer = 0; outer < strlen(accept); outer++)
+for(i = 0; i < n; i++)
 {
-for (i = 0; i < n; i++)
+for (outer = 0; outer < outsize; outer++)
 {
 if (s[i] == accept[outer])
 return (&s[i]);
