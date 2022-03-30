9f51cf376c08a7e13ad267b6e4af90c635b82cb3
Mamo Fideno <mamofideno@gmail.com>
2022-03-30 07:46:48 -0700

    2b

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_print_rev_recursion -prints recu
+ *@s: string to be printed
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+printf("%s\n", strrev(s));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,7 @@
 #define __PUTS_RECURSION_H_
 void _puts_recursion(char *s);
 #endif
+#ifndef _PRINT_REV_RECURSION_H_
+#define _PRIINT_REV_RECURSION_H_
+void _print_rev_recursion(char *s);
+#endif
