6b1e5a8e0a808733fc862e266042a7451c7b6013
Mamo Fideno <mamofideno@gmail.com>
2022-04-05 10:30:07 -0700

    cr

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,17 +10,17 @@ char *str_concat(char *s1, char *s2)
 {
 char *s;
 unsigned int i;
-int l;
-int l2;
+unsigned int l;
+unsigned int l2;
 s = malloc(sizeof(s1) + sizeof(s2)); 
 s = strcat(s1 , s2);
 if (s == NULL)
 return (NULL);
-l = sizeof(s1) / sizeof(char);
-l2 = sizeof(s2) / sizeof(char);
+l = sizeof(s1);
+l2 = sizeof(s2);
 for(i = 0; i < l; i++)
 s[i] = s1[i];
 for(i = 0; i < l2; i++)
-s[i + stlen(s1)] = s2[i];
+s[i + l] = s2[i];
 return (s);
 }
