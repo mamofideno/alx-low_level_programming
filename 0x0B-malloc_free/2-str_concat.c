723674dc438c226e8a7cf697beb5a6755ffdb486
Mamo Fideno <mamofideno@gmail.com>
2022-04-05 10:04:46 -0700

    ls

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 /**
  *str_concat - concat the string
  *@s1: the first string
@@ -8,8 +9,14 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s;
+int i;
+s = malloc(sizeof(s1) + sizeof(s2)); 
 s = strcat(s1 , s2);
 if (s == NULL)
 return (NULL);
+for(i = 0; i < strlen(s1); i++)
+s[i] = s1[i];
+for(i = 0; i < strlen(s2); i++)
+s[i + stlen(s1)] = s2[i];
 return (s);
 }
