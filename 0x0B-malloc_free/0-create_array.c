5c9c0fb1431a30a292d327c269ee6d4f6582298f
Mamo Fideno <mamofideno@gmail.com>
2022-04-05 02:55:17 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
   *create_array - create array of char
   *@size: size of array
   *@c: character
-  *Return: return value 
+  *Return: return value
   */
 char *create_array(unsigned int size, char c)
 {
@@ -13,7 +13,7 @@ if (size == 0)
 return (NULL);
 arr = malloc(sizeof(char) * size);
 if (arr == NULL)
-return (NULL);   
+return (NULL); 
 for (i = 0; i < size; i++)
 arr[i] = c;
 return (arr);
