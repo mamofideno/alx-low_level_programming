40789405bdb251202f3bd776669ce42a7caf4eb2
Mamo Fideno <mamofideno@gmail.com>
2022-04-05 02:38:02 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+
+/**
+ *
+ *create_array- create array of char
+ *@size: size of array
+ *@c: character
+ */
+char *create_array(unsigned int size, char c)
+{
+char *arr;
+if(size == 0)
+return (NULL);
+arr = malloc(sizeof(char) * size);
+for(i = 0; i < size; i++)
+arr[i] = c;
+return (arr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifndef _CREATE_ARRAY_H_
+#define _CREATE_ARRAY_H_
+char *create_array(unsigned int size, char c);
+#endif
