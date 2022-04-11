02f5f9bd81233e17150f5274dbd218d15ebff2ef
Mamo Fideno <mamofideno@gmail.com>
2022-04-11 07:24:14 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ *init_dog - initailize dog
+ *@d: pointer to dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog onwer
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d -> name = name;
+d -> age  = age;
+d -> owner = owner;
+}
