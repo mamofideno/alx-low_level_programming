62bc785c31072809ca8264ed9360f3b15243c4fa
Mamo Fideno <mamofideno@gmail.com>
2022-04-12 06:08:56 -0700

    create

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ *new_dog-create dof 
+ *@name: name
+ *@age : age
+ *@owner: onwer
+ *Return: dog 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t dg;
+dg.name = name;
+dg.age = age;
+dg.owner = owner;
+return (dg);
+}
