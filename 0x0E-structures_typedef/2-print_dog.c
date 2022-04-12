e321c15f0cc09cee9d9256d2b5312707d4a0dc3c
Mamo Fideno <mamofideno@gmail.com>
2022-04-12 05:22:09 -0700

    print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *print_dog - print dog structure
+ *@dog: dog st
+ *Retun: void
+ */
+void print_dog(struct dog *d)
+{
+printf("Name: %s\n",dog->name);
+printf("Age: %f\n",dog->age);
+printf("Owner: %s",dog->owner);
+}
