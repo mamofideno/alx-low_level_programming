f18a4668ad9cc8f7930e5ed820f6bf61d6411326
Mamo Fideno <mamofideno@gmail.com>
2022-04-28 20:22:19 -0700

    mm

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,9 @@
 #include "main.h"
- /**
-  * get_bit - returns the value of a bit at a given
-  * index.
+
+/**
+  * get_bit - returns the value of a bit at a given index.
   * @n: unsigned long int input.
   * @index: index of the bit.
-  *
   *
   * Return: value of the bit.
   */
